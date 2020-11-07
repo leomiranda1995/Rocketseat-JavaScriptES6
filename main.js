6996ea43d50fa1576d752d7ea42ddff2612e04bc
leomiranda1995 <leomiranda1995@gmail.com>
2020-11-07 01:36:20 -0300

    Conceitos ES6 - Classes

--- main.js
+++ main.js
@@ -1,11 +1,42 @@
-alert('Teste');
+// ES6
 
-class Teste {
-    metodo(){
+// Classes
 
+class List {
+    constructor() {
+        this.data = [];
     }
 
-    outrometodo(){
-
+    add(data){
+        this.data.push(data);
+        console.log(this.data);
     }
 }
+
+class TodoList extends  List {
+    constructor() {
+        super();
+
+        this.usuario = 'Leonardo';
+    }
+
+    mostraUsuario(){
+        console.log(this.usuario);
+    }
+
+    // normalmente métodos static, sua classe não tem constructor, pois ele não ve o escopo dela somente para apresentar alguma informação
+    static soma(a, b) {
+        return a + b;
+    }
+}
+
+var MinhaLista = new TodoList();
+
+
+document.querySelector('#novoTodo').onclick = function() {
+    MinhaLista.add('Novo todo');
+}
+
+MinhaLista.mostraUsuario();
+
+console.log(TodoList.soma(6, 7));
