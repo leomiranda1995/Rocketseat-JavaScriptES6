6a8842e4859da77ec0fb192bb849a62314fafdb6
leomiranda1995 <leomiranda1995@gmail.com>
2020-11-09 00:05:00 -0300

    Webpack dev server

--- main.js
+++
@@ -1,7 +0,0 @@
-import soma, { sub as subTeste } from './functions.js';
-import somaFunction from './soma.js';
-
-console.log(soma(1,4));
-console.log(subTeste(8,2));
-
-console.log(somaFunction(7,8));
